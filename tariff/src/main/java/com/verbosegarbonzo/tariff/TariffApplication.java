1c51d40e6507fa45e774f3f5ed243ab5c316fbee
ofwirawan <of.wirawan@gmail.com>
2025-09-12 16:40:53 +0800

    Setup SpringBoot Project

---
+++ tariff/src/main/java/com/verbosegarbonzo/tariff/TariffApplication.java
@@ -0,0 +1,13 @@
+package com.verbosegarbonzo.tariff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TariffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TariffApplication.class, args);
+	}
+
+}
---
+++ tariff/src/test/java/com/verbosegarbonzo/tariff/TariffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.verbosegarbonzo.tariff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TariffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
