c95b79343aff637280b8b07c16d6024dadd7fd9a
achr-is <achristianfrensty@gmail.com>
2025-09-23 09:00:08 +0800

    Add input and output DTOs for frontend

---
+++ tariff/src/main/java/com/verbosegarbonzo/tariff/model/CalculateResponse.java
@@ -0,0 +1,22 @@
+package com.verbosegarbonzo.tariff.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+public class CalculateResponse {
+//Output Data Transfer Object for tariff calculation
+
+    private String hs6;
+    private String reporter;
+    private String partner;
+    private int year;
+    private BigDecimal rateDecimal;  // 0.05 = 5%
+    private BigDecimal tradeValue;
+    private BigDecimal duty;         // tradeValue * rateDecimal
+    private BigDecimal totalPayable; // tradeValue + duty
+    private String dataUrl;          // WITS API URL used for fetching data
+}
